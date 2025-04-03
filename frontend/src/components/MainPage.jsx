2f5b655920365f5cb4239bfcf612b01535e83739
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-03 14:55:36 +0530

    5: created components for login, signup

---
+++ frontend/src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpForm from './SignUpForm'
+
+const MainPage = () => {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  )
+}
+
+export default MainPage
