4209526d39840020eb3ce1fe8abe8dba60fac750
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-04 10:59:56 +0530

    7: defined create, user_params method

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,21 @@ module Api
         @users = User.all
         render json: @users
       end
+
+      def create
+        @user = User.create(user_params)
+        if @user.save
+          render json: @user
+        else
+          render json: {errors: user.errors.full_messages}
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
     end
   end
 end
