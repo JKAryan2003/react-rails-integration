088ad0c76476e3e673bee11403a538858470cb8a
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-04 15:43:46 +0530

    13: sessions controller, token generation

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -8,9 +8,9 @@ module Api
 
       def create
         @user = User.new(user_params)
-        binding.pry
         if @user.save
-          render json: @user
+          token = JWT.encode({user_id: @user.id}, "secretKey", 'HS256')
+          render json: {user: @user, token: token}
         else
           render json: {errors: @user.errors.full_messages}
         end
