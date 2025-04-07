2b6e143694898c3f2e0e50343961ed7e6f276aad
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-04-07 23:17:45 +0530

    16: changed component layout
    changed router, layout, handle logout, defined Users component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,27 @@
 import './App.css'
 import MainPage from './components/MainPage'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import SignUpForm from './components/SignUpForm';
 import LoginForm from './components/LoginForm';
 import Users from './components/Users';
 import NavBar from './components/NavBar';
+import { useState } from 'react';
 
 function App() {
-
-  const router = createBrowserRouter(
-    [ 
-      {
-        path: '/',
-        element:
-        <div>
-          <NavBar />
-          <MainPage />
-        </div>
-      },
-
-      {
-        path: '/users',
-        element: 
-        <div>
-          <NavBar />
-          <Users />
-        </div>
-      },
-
-      {
-        path: '/signup',
-        element: 
-        <div>
-          <NavBar />
-          <SignUpForm />
-        </div>
-      },
-      
-      {
-        path: '/login',
-        element: 
-        <div>
-          <NavBar />
-          <LoginForm />
-        </div>
-      }
-    ]
-  )
- 
+  const [token, setToken] = useState()
   return (
     <>
-      <RouterProvider router={router}>
-        <NavBar />
-      </RouterProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NavBar token={token} setToken={setToken}/>}>
+            <Route index element={<MainPage />} />
+            <Route path="users" element={<Users />} />
+            <Route path="signup" element={<SignUpForm />} />
+            <Route path="login" element={<LoginForm token={token} setToken={setToken}/>} />
+            {/* <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,24 +1,7 @@
 import React from 'react'
 import SignUpForm from './SignUpForm'
-import axios from 'axios'
-import { useEffect } from 'react'
 
 const MainPage = () => {
-
-  const url = "http://127.0.0.1:3000/api/v1/users"
-
-  const getData = async () => {
-    await axios.get(url)
-    .then(response => console.log(response)
-    )
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
-
-
-  
   return (
     <div>
       This is Home
